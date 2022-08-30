213f89c01f972efff9a3b6ab5e035aff71861b50
Francois Soulier <francois.soulier@epita.fr>
2022-08-30 19:12:29 +0200

    servers-tests: added tests for basic day routes

--- test/server/testBasicDaysBookingsAvailabilities.ts
+++ test/server/testBasicDaysBookingsAvailabilities.ts
@@ -1,28 +1,27 @@
 import { Server } from '../../src/server/middlewareServer.js';
-import request from 'supertest';
 import { AppConstants } from "../../src/utils/appConstants.js";
+import request from 'supertest';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED='0'
 
 let server = new Server().shinkoMiddleware()
 
-describe(`GET ${AppConstants.Server.Routes.basicBookingsAvailabilities}`, () => {
+describe(`GET ${AppConstants.Server.Routes.basicDaysBookingsAvailabilities}`, () => {
     it('Should return scraped dates', (done) => {
         request(server)
-            .get(AppConstants.Server.Routes.basicBookingsAvailabilities)
+            .get(AppConstants.Server.Routes.basicDaysBookingsAvailabilities)
             .expect(200)
             .end(done)
     }).timeout(0)
 });
 
-describe(`GET ${AppConstants.Server.Routes.basicBookingsAvailabilities}`, () => {
+describe(`GET ${AppConstants.Server.Routes.basicDaysBookingsAvailabilities}`, () => {
     it('Should return cached dates', (done) => {
         request(server)
-            .get(AppConstants.Server.Routes.basicBookingsAvailabilities)
+            .get(AppConstants.Server.Routes.basicDaysBookingsAvailabilities)
             .expect(200)
             .end(done)
     })
 });
 
 server.close()
-
