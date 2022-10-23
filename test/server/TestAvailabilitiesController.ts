1b35a343f049b29a385af9c36ec359fd4c2b72d0
Francois Soulier <francois.soulier@epita.fr>
2022-10-23 20:05:18 +0200

    [test] Updated testsuite to new server

---
+++ test/server/TestAvailabilitiesController.ts
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import { AppConstants } from "../../src/Utils/AppConstants.js";
+import {ServerBuilder} from "../../src/ServerBuilder.js";
+
+let server = ServerBuilder.build()
+ServerBuilder.start(server)
+
+describe(`GET ${AppConstants.Server.Routes.getAllAvailabilities}`, () => {
+    it('Should return all available bookings from Shinko', (done) => {
+        request(server)
+            .get(AppConstants.Server.Routes.getAllAvailabilities)
+            // .trustLocalhost()
+            .expect(200)
+            .end(done)
+    })
+});
+
+server.close()
