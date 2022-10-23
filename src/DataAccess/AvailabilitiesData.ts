0999ce03b6e20e19b00a9f93fdcdf2bfe87cd440
Francois Soulier <francois.soulier@epita.fr>
2022-10-23 15:52:40 +0200

    [AvailabilitiesData] Adjusted coding style

--- src/DataAccess/AvailabilitiesData.ts
+++ src/DataAccess/AvailabilitiesData.ts
@@ -8,12 +8,12 @@ import { AvailabilitiesResponse } from "../Models/AvailabilitiesResponse.js";
 
 class AvailabilitiesData {
     /**
-     * Fetch availabilities JSON
+     * Fetch availabilities by begin and en date under JSON format
      * @param {string} beginDate - Begin date in YYYY-MM-DD format
      * @param {string} endDate - End date in YYYY-MM-DD format
      * @returns {Promise<string>} - Promise object represents the availabilities JSON
      */
-    static async getAvailabilities(beginDate: string, endDate: string): Promise<AvailabilitiesResponse[]> {
+    static async getAvailabilitiesByDate(beginDate: string, endDate: string): Promise<AvailabilitiesResponse[]> {
         const response = await fetch(AppConstants.Shinko.availabilitiesURL(beginDate, endDate))
         let responseJSON = await response.text()
         return JSON.parse(responseJSON)
