f7f1e1309af08322fa28fc83490729d5835a7188
Francois Soulier <francois.soulier@epita.fr>
2022-10-23 20:15:02 +0200

    Removed environment variables

--- set_environment_variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo export CACHE_DURATION="300";
-echo export SERVER_PORT="3000";
-echo export SHINKO_BASE_URL="https://www.shinkoparis.fr";
-echo export CERTIFICATION_PATH="certs";
--- src/Utils/AppConstants.ts
+++ src/Utils/AppConstants.ts
@@ -1,11 +1,5 @@
-const environment = process.env
-
 class AppConstants {
     static Shinko = class {
-        /**
-         * Shinko base URL
-         */
-        static baseURL: string = environment.SHINKO_BASE_URL
 
         /**
          * Booking ZenChef base URL
@@ -29,25 +23,11 @@ class AppConstants {
         }
     }
 
-    static Cache = class {
-        /**
-         * Booking dates cache key
-         */
-        static bookingDatesKey: string = "bookingDatesKey"
-
-        static bookingDaysKey: string = "bookingDaysKey"
-    }
-
     static Server = class {
         /**
          * Server port
          */
-        static port: number = Number(environment.SERVER_PORT || '3000')
-
-        /**
-         * Certification base path
-         */
-        static certifcationBasePath: string = environment.CERTIFICATION_PATH || 'certs'
+        static port: number = 3000
 
         static Routes = class {
             /**
@@ -64,16 +44,6 @@ class AppConstants {
              * Get availabilities between dates route
              */
             static getAvailabilitiesBetweenDates: string = '/getAvailabilitiesBetweenDates'
-
-            /**
-             * Basic bookings availabilities route
-             */
-            static basicBookingsAvailabilities: string = '/basicBookingsAvailabilities'
-
-            /**
-             * Basic days bookings availabilities route
-             */
-            static basicDaysBookingsAvailabilities: string = '/basicDaysBookingsAvailabilities'
         }
     }
 }
